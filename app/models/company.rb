37215ada093783f884df7b6e701b48da4a7a8fd0
gglin <george.g.lin@gmail.com>
2013-08-16 18:59:57 -0400

    Simple form

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
 class Company < ActiveRecord::Base
   attr_accessible :crunchbase_url, :founded_year, :glassdoor_url, :linkedin_url, :name, :number_of_employees, :overview, :twitter_username, :website
+
+  validates :name, presence: true
 end
