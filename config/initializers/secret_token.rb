78eed22031b5219ce9b9d47b993a85c231ca7ae7
gglin <george.g.lin@gmail.com>
2013-08-16 16:29:05 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobbit::Application.config.secret_token = '1648e73096a151b4789219b573a73d8bd241bb2549df5d4bb47db8b174cdc606d47471019e60694adf1c47f09f81df6409e31716edabd42d18543937ef9412da'
