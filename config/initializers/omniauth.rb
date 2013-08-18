9da5a9bb8151ce94bb408de16cd4f96b5a9c66b2
gglin <george.g.lin@gmail.com>
2013-08-18 17:20:04 -0400

    Linkedin Fields specified for Omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,8 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
   provider :linkedin, ENV["LINKEDIN_KEY"], ENV["LINKEDIN_SECRET"], 
-           :scope => 'r_fullprofile r_emailaddress r_network r_contactinfo' 
-           # :state => params[:state]
+           :scope => 'r_fullprofile r_emailaddress r_network', :fields => ["id", "email-address", "first-name", "last-name", "headline", "industry", "picture-url", "public-profile-url", "location", "connections"]
+           
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
            # :state => params[:state]
 end
